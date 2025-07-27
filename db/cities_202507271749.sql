74fdc4b490df6276780087131a8082b6cf089aa8
🐉☢ <ferhat.ulugoel@gmail.com>
2025-07-27 18:00:22 +0200

    Add Documentation and SQL Tables

---
+++ db/cities_202507271749.sql
@@ -0,0 +1,11 @@
+INSERT INTO lotgd.cities (cityname,citytype,cityauthor,cityactive,citychat,citytravel,citytext,stabletext,armortext,weaponstext,mercenarycamptext,cityblockmods,cityblocknavs,module) VALUES
+	 ('Village of Avalon','Village','Demonmegi',1,0,0,'a:2:{s:5:"title";s:17:"Village of Avalon";s:4:"text";s:59:"This is the village of Avalon, south of the Citadel itself.";}','','','','','','a:2:{s:7:"weapons";s:1:"1";s:5:"armor";s:1:"1";}','city_creator'),
+	 ('Southgate Avalon','City','Demonmegi',1,0,0,'a:2:{s:5:"title";s:16:"Southgate Avalon";s:4:"text";s:143:"The Southgate is still heavily damaged by the Siege which concluded 1 year ago. Due to the ongoing repairs, you won\\''t be able to enter Avalon.";}','','','','','','a:17:{s:6:"forest";s:1:"1";s:3:"pvp";s:1:"1";s:13:"mercenarycamp";s:1:"1";s:5:"train";s:1:"1";s:5:"lodge";s:1:"1";s:7:"weapons";s:1:"1";s:5:"armor";s:1:"1";s:4:"bank";s:1:"1";s:5:"gypsy";s:1:"1";s:3:"inn";s:1:"1";s:7:"stables";s:1:"1";s:7:"gardens";s:1:"1";s:4:"rock";s:1:"1";s:4:"clan";s:1:"1";s:4:"news";s:1:"1";s:4:"list";s:1:"1";s:3:"hof";s:1:"1";}','city_creator'),
+	 ('Outer Bailey','City','Demonmegi',1,0,0,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'city_creator'),
+	 ('Peasants Bailey','Village','Demonmegi',1,0,0,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'city_creator'),
+	 ('Outer Bailey','City','Demonmegi',1,0,0,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'city_creator'),
+	 ('Inner Bailey','City','Demonmegi',1,0,0,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'city_creator'),
+	 ('Inner Keep','City','Demonmegi',1,0,0,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'city_creator'),
+	 ('Caves','City','Demonmegi',1,0,0,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'city_creator'),
+	 ('Crypt','City','Demonmegi',1,0,0,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'city_creator'),
+	 ('The Steppe','City','Demonmegi',1,0,0,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'city_creator');
